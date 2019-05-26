5bd2774d9ce980eeaab8e9a7a95e26e6c2856392
luckychris <lucky_chris_again@yahoo.de>
2019-05-26 10:20:13 +0200

    Update UITableViewVibrantCell.swift

--- Pod/Classes/UITableViewVibrantCell.swift
+++ Pod/Classes/UITableViewVibrantCell.swift
@@ -10,12 +10,23 @@ import UIKit
 
 open class UITableViewVibrantCell: UITableViewCell {
     
+    var tapGR : UITapGestureRecognizer?
+
     fileprivate var vibrancyView:UIVisualEffectView = UIVisualEffectView()
     fileprivate var vibrancySelectedBackgroundView:UIVisualEffectView = UIVisualEffectView()
     fileprivate var defaultSelectedBackgroundView:UIView?
-    open var blurEffectStyle: UIBlurEffect.Style? {
+    open var blurEffectStyle: UIBlurEffect.Style = .light {
         didSet {
             updateBlur()
+        }
+    }
+    
+    @IBInspectable var blurEffectStyleIB: Int {
+        get {
+            return blurEffectStyle.rawValue
+        }
+        set {
+            self.blurEffectStyle = UIBlurEffect.Style(rawValue: newValue)!
         }
     }
     
@@ -39,13 +50,31 @@ open class UITableViewVibrantCell: UITableViewCell {
         defaultSelectedBackgroundView = selectedBackgroundView
         
         updateBlur()
+        
+        self.tapGR = UITapGestureRecognizer(target: self, action: #selector(self.cellTapped(tapGR:)))
+        tapGR?.cancelsTouchesInView = false
+        self.addGestureRecognizer(tapGR!)
+    }
+    
+    @objc func cellTapped(tapGR: UITapGestureRecognizer) {
+        
+        if contentView.subviews.count > 0 {
+            
+            if let cellID = reuseIdentifier {
+                
+       //         if SharedData.shared.allowedReuseIDsToSendNotifications.contains(cellID) {
+                    
+                    NotificationCenter.default.post(name: NSNotification.Name(rawValue: cellID), object: nil, userInfo: ["cellID":cellID])
+    //            }
+            }
+        }
     }
     
     internal func updateBlur() {
         // shouldn't be needed but backgroundColor is set to white on iPad:
         backgroundColor = UIColor.clear
         
-        if let blurEffectStyle = blurEffectStyle, !UIAccessibility.isReduceTransparencyEnabled {
+        if  !UIAccessibility.isReduceTransparencyEnabled {
             let blurEffect = UIBlurEffect(style: blurEffectStyle)
             vibrancyView.effect = UIVibrancyEffect(blurEffect: blurEffect)
             
